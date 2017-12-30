c939aae441913408f7afb51033af44ddda3510ac
emsambit <emsambit@gmail.com>
2017-12-30 23:23:20 +0530

    This is to create the Database Library for the images.
    Database will hold the Images from webcam with different front face movement , it will capture 45 images.

---
+++ Create_Database_test.py
@@ -0,0 +1,56 @@
+import cv2, sys, numpy, os, time
+count = 0
+size = 4
+fn_haar = 'haarcascade_frontalface_default.xml'
+fn_dir = 'database'
+fn_name = sys.argv[ 1 ]  # name of the person
+path = os.path.join ( fn_dir, fn_name )
+if not os.path.isdir ( path ):
+    os.mkdir ( path )
+(im_width, im_height) =(224,224)
+    #(112, 92)
+haar_cascade = cv2.CascadeClassifier ( fn_haar )
+# for USB webcam the value is 1 and for laptop camera the value is 0
+webcam = cv2.VideoCapture (0)
+
+print ( "-----------------------Taking pictures----------------------" )
+
+# The program loops until it has 20 images of the face.
+
+while count < 45:
+    (rval, im) = webcam.read ()
+    im = cv2.flip ( im, 1, 0 )
+    gray = cv2.cvtColor ( im, cv2.COLOR_BGR2GRAY )
+    x=(int(gray.shape[ 1 ] / size))
+    y=(int(gray.shape[ 0 ] / size))
+    #print (x)
+    #print (y)
+    #mini = cv2.resize ( gray, (gray.shape[ 1 ] / size, gray.shape[ 0 ] / size) )
+    #mini =  cv2.resize (gray,(100,50))
+    mini = cv2.resize ( gray, (x, y) )
+    faces = haar_cascade.detectMultiScale ( mini )
+    faces = sorted ( faces, key=lambda x: x[ 3 ] )
+
+    if faces:
+        face_i = faces[0]
+        (x, y, w, h) = [v * size for v in face_i]
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (im_width, im_height))
+        pin=sorted([int(n[:n.find('.')])for n in os.listdir(path)
+               if n[0]!='.' ]+[0])[-1] + 1
+        cv2.imwrite ( '%s/%s.png' % (path, pin), face_resize )
+        #cv2.imwrite('%s/%s.png' % (path, pin), gray[y:y+h,x:x+w ])
+        cv2.rectangle ( im, (x, y), (x + w, y + h), (0, 255, 0), 3 )
+        cv2.putText ( im, fn_name, (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN,
+                      1, (0, 255, 0) )
+        time.sleep ( 0.38 )
+    count = count+1
+
+    cv2.imshow ( 'OpenCV', im )
+    key = cv2.waitKey ( 10 )
+    if key == 27:
+        break
+print (str(count) + " images taken and saved to " + fn_name +" folder in database ")
+
+webcam.release ()
+cv2.destroyAllWindows ()
